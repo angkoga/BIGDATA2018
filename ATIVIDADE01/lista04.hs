3d911c3b0666f586a46f5cb48df689c72a3d1bdb
angkoga <angelica.koga@gmail.com>
2018-03-31 12:35:19 -0300

    teste com windows

---
+++ ATIVIDADE01/lista04.hs
@@ -0,0 +1,58 @@
+--lista 04
+--Angélica Koga
+
+--Separando osFunções
+--exercício01
+
+
+--exercício02
+
+
+--exercício03
+
+
+--exercício04
+
+
+
+--Separando os Prints 
+main = do
+
+----pular linha
+  putStrLn " "
+--exercício01
+  print ("exercicio_01")
+  print ( )
+  
+----pular linha
+  putStrLn " "
+--exercício02
+  print ("exercicio_02")
+  print ( )
+    
+----pular linha
+  putStrLn " "
+--exercício03
+  print ("exercicio_03")
+    print ( )
+  
+----pular linha
+  putStrLn " "
+--exercício04
+  print ("exercicio_04")
+    print ( )
+  
+----pular linha
+  putStrLn " "  
+--exercício05
+  print ("exercicio_05")
+    print ( )
+  
+----pular linha
+  putStrLn " "  
+--exercício06
+  print ("exercicio_06")
+  print ( )
+  
+----pular linha
+  putStrLn " "
