566a72a54a856f13b50cfb9ca96b1f548206606d
angkoga <angelica.koga@gmail.com>
2018-04-01 14:17:38 -0300

    exercicios atividade01

--- ATIVIDADE01/lista02.hs
+++ ATIVIDADE01/lista02.hs
@@ -1,58 +1,62 @@
---lista 02
---Angélica Koga
+--lista 02 -- Angélica Koga
+--module Main where
 
---Separando osFunções
---exercício01
+--Separando as Funções
+--Exercício 01: Crie uma função ehTriangulo que determina se três lados x, y, z podem formar um triângulo.
+
+--Exercício 02: Crie uma função tipoTriangulo que determina o tipo do triângulo formado pelos três lados x, y, z.
+
+--Exercício 03: Implemente uma função que faz a multiplicação etíope entre dois números.
+
+--Exercício 04: Faça uma função que determine se um número é primo.
+
+--Exercício 05: Faça uma função que calcule a soma dos dígitos de um número.
+
+--Exercício 06: Faça uma função que calcule a persistência aditiva de um número.
+
+--Exercício 07: Faça uma função que calcule o coeficiente binomial de (m,n).
+
+--Exercício 08: Faça uma função que calcule o elemento (i,j) do triângulo de pascal.
 
 
---exercício02
-
-
---exercício03
-
-
---exercício04
 
 
 
 --Separando os Prints 
+main :: IO()
 main = do
-
+  print ("Lista 02- "++ "Angelica Satiko Koga")
 ----pular linha
   putStrLn " "
---exercício01
   print ("exercicio_01")
   print ( )
   
-----pular linha
   putStrLn " "
---exercício02
   print ("exercicio_02")
   print ( )
     
-----pular linha
   putStrLn " "
---exercício03
   print ("exercicio_03")
     print ( )
   
-----pular linha
   putStrLn " "
---exercício04
   print ("exercicio_04")
     print ( )
   
-----pular linha
   putStrLn " "  
---exercício05
   print ("exercicio_05")
     print ( )
   
-----pular linha
   putStrLn " "  
---exercício06
   print ("exercicio_06")
   print ( )
+
+  putStrLn " "  
+  print ("exercicio_07")
+  print ( )
   
-----pular linha
+  putStrLn " "  
+  print ("exercicio_08")
+  print ( )
+    
   putStrLn " "
