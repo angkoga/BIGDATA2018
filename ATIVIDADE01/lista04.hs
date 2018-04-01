95859193349bb17664ee7a3d8190d8b7afbded72
angkoga <angelica.koga@gmail.com>
2018-04-01 14:19:10 -0300

    exercicios atividade01

--- ATIVIDADE01/lista04.hs
+++ ATIVIDADE01/lista04.hs
@@ -1,58 +1,30 @@
---lista 04
---Angélica Koga
+--lista 04 -- Angélica Koga
+--module Main where
 
---Separando osFunções
---exercício01
+--Separando as Funções
+--Exercício 01: Faça uma função que gere uma matriz identidade de tamanho n.
 
+--Exercício 02: Faça uma função que calcule a soma da diagonal principal de uma matriz.
 
---exercício02
-
-
---exercício03
-
-
---exercício04
+--Exercício 03: Faça uma função que calcule a soma da diagonal secundária de uma matriz.
 
 
 
 --Separando os Prints 
+main :: IO()
 main = do
-
+  print ("Lista 04- "++ "Angelica Satiko Koga")
 ----pular linha
   putStrLn " "
---exercício01
   print ("exercicio_01")
   print ( )
   
-----pular linha
   putStrLn " "
---exercício02
   print ("exercicio_02")
   print ( )
     
-----pular linha
   putStrLn " "
---exercício03
   print ("exercicio_03")
     print ( )
-  
-----pular linha
-  putStrLn " "
---exercício04
-  print ("exercicio_04")
-    print ( )
-  
-----pular linha
-  putStrLn " "  
---exercício05
-  print ("exercicio_05")
-    print ( )
-  
-----pular linha
-  putStrLn " "  
---exercício06
-  print ("exercicio_06")
-  print ( )
-  
-----pular linha
+   
   putStrLn " "
