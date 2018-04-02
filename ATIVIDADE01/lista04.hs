bc0e73b61c8c0df1dbf0afa96c898421d9c67849
angkoga <angelica.koga@gmail.com>
2018-04-02 13:06:38 -0300

    exercicios atividade01

--- ATIVIDADE01/lista04.hs
+++ ATIVIDADE01/lista04.hs
@@ -3,28 +3,38 @@
 
 --Separando as Funções
 --Exercício 01: Faça uma função que gere uma matriz identidade de tamanho n.
+createMatrix :: Integer -> [[Integer]]
+createMatrix size =  [[if (y == x) then 1 else 0 | x <- [1..size]] | y <- [1..size]]
+
 
 --Exercício 02: Faça uma função que calcule a soma da diagonal principal de uma matriz.
+getFirstDiag matrix = zipWith (!!) matrix [0..]
+sumFirstDiag matrix = foldl (+) 0 (getFirstDiag matrix)
+  
 
 --Exercício 03: Faça uma função que calcule a soma da diagonal secundária de uma matriz.
+getSecondDiag matrix = zipWith (!!) matrix [length matrix-1, length matrix-2..]
+sumSecondDiag matrix = foldl (+) 0 (getSecondDiag matrix)
 
 
 
 --Separando os Prints 
 main :: IO()
 main = do
-  print ("Lista 04- "++ "Angelica Satiko Koga")
+  print ("Lista 04- " ++ "Angelica Satiko Koga")
 ----pular linha
   putStrLn " "
   print ("Exercicio 01:")
-  print ( )
+  print (createMatrix 9)
   
   putStrLn " "
   print ("Exercicio 02:")
-  print ( )
-    
+  let matrix = createMatrix 5
+  print (matrix)
+  print (sumFirstDiag matrix)
+      
   putStrLn " "
   print ("Exercicio 03:")
-    print ( )
-   
+  print (getSecondDiag matrix)
+  print (sumSecondDiag matrix)
   putStrLn " "
